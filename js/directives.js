1e9770170e3707ec9231205a4293e3518ca3f4d7
Ivan Ivanov <ipivanov11@gmail.com>
2015-05-27 18:27:42 +0300

    Update

--- js/directives.js
+++ js/directives.js
@@ -1,15 +1,21 @@
 twitchApp.directive('twitchVideo',[function(){
   return{
     restrict: 'E',
-    template: '<div>......</div>', // your container
+    template: '<div>' +streamName+'</div>', // your container
     scope: {
       stream: '='
     },
-    link: function($scope, element, attrs){
-        $scope.$watch(attrs.stream, function(value) {
+    link: function(scope, element, attrs){
+        scope.$watch(attrs.stream, function(value) {
            element.html(''); // clear any content
            if(value){
-                element.html('your resulting object tag with the stream name received');
+                element.html('<object type="application/x-shockwave-flash" height="378" width="620" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">'+
+					'<param name="allowFullScreen" value="true">'+
+					'<param name="allowScriptAccess" value="always">'+
+					'<param name="allowNetworking" value="all">'+
+					'<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf">'+
+					'<param name="flashvars" value="channel='+ streamName + '&amp;auto_play=true&amp;start_volume=100">'+
+				'</object>');
            }
         });
     }
