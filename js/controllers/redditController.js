d9e9a55d0ae662a75fb4e0816c35a27c7b92f808
Ivan Ivanov <ipivanov11@gmail.com>
2015-05-31 02:42:33 +0300

    Adding new and rising to the feed

--- js/controllers/redditController.js
+++ js/controllers/redditController.js
@@ -1,8 +1,28 @@
 'use strict';
-twitchApp.controller('redditController', ['$scope', 'Reddit', 'PageTitle', function($scope, Reddit, PageTitle ) {
+twitchApp.controller('redditController', ['$scope', 'RedditHot', 'RedditNew', 'RedditRising', 'PageTitle', function($scope, RedditHot, RedditNew, RedditRising, PageTitle ) {
+	
+	$scope.redditHot = new RedditHot();
+	$scope.redditNew = new RedditNew();
+	$scope.redditRising = new RedditRising();
+	$scope.showHot = function() {
+		$scope.toggleVar1 = true;
+		$scope.toggleVar2 = false;
+		$scope.toggleVar3 = false;
+		$scope.hideFirst = true;
+	};
+    $scope.showNew = function() {
+		$scope.toggleVar1 = false;
+		$scope.toggleVar2 = true;
+		$scope.toggleVar3 = false;
+		$scope.hideFirst = true;
+	};
+	$scope.showRising = function() {
+		$scope.toggleVar1 = false;
+		$scope.toggleVar2 = false;
+		$scope.toggleVar3 = true;
+		$scope.hideFirst = true;
+	};
 
-
-    $scope.reddit = new Reddit();
 	PageTitle.setTitle('Reddit News');
 
 
