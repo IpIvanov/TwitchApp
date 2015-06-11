019035a1cf6ac583e46428de3b90a81c64ce0a97
Ivan Ivanov <ipivanov11@gmail.com>
2015-06-11 18:33:34 +0300

    Added timout function to refresh data

--- js/controllers/streamController.js
+++ js/controllers/streamController.js
@@ -6,10 +6,25 @@ twitchApp.controller('streamController', ['$scope', '$http', '$location' , funct
 	    $scope.data = data;
 	    $scope.url = data.stream.channel.url + '/embed';
 	    $scope.chatUrl = data.stream.channel.url + '/chat';
+	   
 	  }).
 	  error(function(data, status, headers, config) {
 	    // called asynchronously if an error occurs
 	    // or server returns response with an error status.
 	  });
+	$scope.getData = function(){
+		$http.jsonp('https://api.twitch.tv/kraken/streams/' + streamName + '?callback=JSON_CALLBACK').
+	  success(function(data, status, headers, config) {
+	    $scope.data = data;
+	    $scope.url = data.stream.channel.url + '/embed';
+	    $scope.chatUrl = data.stream.channel.url + '/chat';
+	   
+	  }).
+	  error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+	};
+	setInterval($scope.getData, 10000);
 }]);
 
